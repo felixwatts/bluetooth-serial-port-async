dfe9c530f611248ed18314eda2c0fab124fb4ca6
Felix Watts <felixwatts@gmail.com>
2024-06-28 10:18:41 +0100

    improve examples

--- examples/example.rs
+++ examples/example.rs
@@ -1,16 +1,24 @@
-#[cfg(any(feature = "tokio", not(feature = "async_std")))]
-compile_error!("this example must be compiled with `--features async_std` or default features");
-
+#[cfg(feature="async_std")]
 use async_std::io::prelude::*;
-use bluetooth_serial_port_async::{BtProtocol, BtSocket};
-use std::time;
 
+#[cfg(feature="tokio")]
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+use bluetooth_serial_port_async::{BtProtocol, BtSocket};
+
+#[cfg(feature="async_std")]
 fn main() {
     async_std::task::block_on(run());
 }
 
+#[cfg(feature="tokio")]
+#[tokio::main]
+async fn main() {
+    run().await
+}
+
 async fn run() {
-    let devices = bluetooth_serial_port_async::scan_devices(time::Duration::from_secs(20)).unwrap();
+    let devices = bluetooth_serial_port_async::scan_devices(std::time::Duration::from_secs(20)).unwrap();
     if devices.len() == 0 {
         panic!("No devices found");
     }
@@ -28,7 +36,7 @@ async fn run() {
     // create and connect the RFCOMM socket
     let mut socket = BtSocket::new(BtProtocol::RFCOMM).unwrap();
     socket.connect(device.addr).unwrap();
-
+    
     let mut buffer = [0; 10];
     // BtSocket::get_stream returns an async stream
     let mut stream = socket.get_stream();
